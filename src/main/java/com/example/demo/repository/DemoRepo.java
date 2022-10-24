a4a1c3e3e77d4f6c0a41a67b6f330906959c4994
MVijayKrishna <vijaykrishna.mundrathi@gmail.com>
2022-10-24 15:47:42 +0530

    3 rd commit.....

---
+++ src/main/java/com/example/demo/repository/DemoRepo.java
@@ -0,0 +1,22 @@
+package com.example.demo.repository;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class DemoRepo {
+	@Value("${Age}")
+	private String Age;
+
+	@Value("${Salary}")
+	private String Salary;
+
+	@Value("${Name}")
+	private String Name;
+
+	public String hello() {
+
+		return Name + " age is = " + Age + " and salary is =" + " " + Salary;
+	}
+
+}
